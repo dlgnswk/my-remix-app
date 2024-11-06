8726f577733661b2ec536903b25d2c498d736c72
ljhoon <dlwogns5140@gmail.com>
2024-11-06 15:31:00 +0900

    Design: Header 알림 컴포넌트 구현

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,16 @@
+import { Alert, AlertTitle, AlertDescription } from "./ui/alert";
+import { Terminal } from "lucide-react";
+
 interface HeaderProps {
   message: string;
 }
 
 export default function Header({ message }: HeaderProps) {
-  return <h1>{message}</h1>;
+  return (
+    <Alert>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>잠시만요!</AlertTitle>
+      <AlertDescription>테스트 단계입니다.</AlertDescription>
+    </Alert>
+  );
 }
